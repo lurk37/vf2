cc53cacc44e9b8b58ce39e88399aef2306d356d9
master <master@lurkui-MacBookPro.local>
2020-08-12 01:26:58 +0900

    12-commit

---
+++ src/views/site/menu.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+      <v-list>
+      <v-list-group
+        v-for="item in items"
+        :key="item.title"
+        v-model="item.active"
+        :prepend-icon="item.action"
+        no-action
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for="subItem in item.items"
+          :key="subItem.title"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subItem.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+</div>
+</template>
+<script>
+export default {
+  props: ['footer'],
+  data () {
+    return {
+      item: [
+        {
+          title: 'home',
+          to: '/'
+        },
+        {
+          title: 'help',
+          to: '/help'
+        }
+      ]
+    }
+  }
+}
+</script>
